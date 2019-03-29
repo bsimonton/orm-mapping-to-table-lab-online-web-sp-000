188730b344a7816ad7e8a361dbb6642061153926
Brian Simonton <bsimonton78@gmail.com>
2019-03-29 19:43:50 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -9,11 +9,7 @@ def initialize(name, grade, id=nil)
   end  
   
   
-  def self.create_table
-  sql = <<-SQL CREATE TABLE IF NOT EXISTS students (id INTEGER PRIMARY KEY, name TEXT, grade TEXT)SQL
-  DB[:conn].execute(sql)
   
-end 
   
   
   
